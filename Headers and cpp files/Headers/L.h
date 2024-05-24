0faf6aedd98abbbae4121c5cdb6591eb2f9b1316
Martin Hjulstad Bednar <martinhb@student.uia.no>
2024-05-24 08:00:21 +0200

    Rotations fixed

--- Headers and cpp files/Headers/L.h
+++ Headers and cpp files/Headers/L.h
@@ -8,7 +8,6 @@ public:
     L(const sf::Texture &texture, std::vector<sf::Vector2f>& positions, int size, int x, int y, int playerID, int& score);
     void rotateClockWise() override;
     void rotateCounterClockWise() override;
-    void rotate() override; //empty implementation since the class is derived from basepiece
 };
 
 #endif //TETRIS_SFML_L_H
--- Headers and cpp files/Headers/S.h
+++ Headers and cpp files/Headers/S.h
@@ -6,8 +6,6 @@
 class S : public BasePiece {
 public:
     S(const sf::Texture &texture, std::vector<sf::Vector2f>& positions, int size, int x, int y, int playerID, int& score);
-    void rotateClockWise() override; //empty implementation
-    void rotateCounterClockWise() override; //empty implementation
     void rotate() override;
 
 private:
